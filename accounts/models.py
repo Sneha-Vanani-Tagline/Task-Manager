3c8b26350baa177c5f107c6f3f0c1db748ea38a6
Mac <mac@Taglines-iMac-Pro-36.local>
2026-07-14 11:46:46 +0530

    initial setup

---
+++ accounts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# class User(models.Model):
+#     class UserOption(models.TextChoices):
+
+#     name = models.CharField(max_length=100)
+#     email = models.EmailField()
+#     password = models.CharField(max_length=300)
+#     role = models.CharField(max_length=60)
+#     last_login = models.DateTimeField()
+#     date_joined = models.DateTimeField()
+
